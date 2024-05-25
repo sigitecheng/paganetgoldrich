cddd195ea62495b4de8212b72282901b5363be5c
Sigimatika <sigitechindo@gmail.com>
2024-05-25 18:28:07 +0700

    hjhj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,10 @@
                                     </svg>
                                 </div>
 
+
+                                WEB SEDANG DALAM PERBAIKAN HARAP MENUNGGU !
+                                SELAMAT DATANG 
+
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
