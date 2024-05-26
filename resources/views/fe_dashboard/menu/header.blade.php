db65cbc0ed4cca8e96ac38d6a03c1d2058c93cfc
Sigimatika <sigitechindo@gmail.com>
2024-05-26 17:43:25 +0700

    hhh

--- resources/views/fe_dashboard/menu/header.blade.php
+++ resources/views/fe_dashboard/menu/header.blade.php
@@ -7,4 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/fe_css/js/flickity.min.css" media="screen">
+    <title>{{ $title }}</title>
+    <!-- Ganti dengan lokasi yang benar sesuai dengan struktur folder proyek Anda -->
+    <link rel="icon" type="image/png" href="assets/css/fe_css/images/icons/menuandroid/logohaiufull.jpg">
 </head>
