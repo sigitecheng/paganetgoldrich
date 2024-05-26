891485a149113aead3264c79d5f3fb358b36b34e
Sigimatika <sigitechindo@gmail.com>
2024-05-26 16:18:53 +0700

    gg

--- resources/views/fe_dashboard/menu/android.blade.php
+++ resources/views/fe_dashboard/menu/android.blade.php
@@ -1,31 +1,31 @@
 <div id="menu" style="max-width: 341px; width: 100%; position: fixed; bottom: 20px; padding: 0.75rem; display: flex; align-items: center; justify-content: space-between; border-radius: 30px; background-image: linear-gradient(to bottom, #8fd1e7, #0000ff); left: 50%; transform: translateX(-50%);">
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 1px solid white; border-radius: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="assets/css/fe_css/images/icons/menuandroid/menudonasi.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
   
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 1px solid white; border-radius: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="assets/css/fe_css/images/icons/menuandroid/menuprogrammakangratis.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 1px solid white; border-radius: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="assets/css/fe_css/images/icons/menuandroid/menucamera.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 1px solid white; border-radius: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="assets/css/fe_css/images/icons/menuandroid/menusettings.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
   
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 1px solid white; border-radius: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="assets/css/fe_css/images/icons/menuandroid/menuback.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
