94580a67db2edd8963fe5810c2a2251af50d612f
Sigimatika <sigitechindo@gmail.com>
2024-05-28 23:56:12 +0700

    ini lagu lafegeg

--- resources/views/fe_dashboard/menu/android.blade.php
+++ resources/views/fe_dashboard/menu/android.blade.php
@@ -1,31 +1,31 @@
-<div id="menu" style="max-width: 341px; width: 100%; position: fixed; bottom: 20px; padding: 0.75rem; display: flex; align-items: center; justify-content: space-between; border-radius: 30px; background-image: linear-gradient(to bottom, #8fd1e7, #0000ff); left: 50%; transform: translateX(-50%);">
+<div id="menu" style="max-width: 350px; width: 100%; position: fixed; bottom: 20px; padding: 0.75rem; display: flex; align-items: center; justify-content: space-between; border-radius: 20px; background-image: linear-gradient(to bottom, #38b5df, #2c2cbe); left: 50%; transform: translateX(-50%);">
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 2px solid white; border-radius: 25%; solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/newmenudonasi.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+        <div style=" solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/css/fe_css/images/icons/menuandroid/appdonasi.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
   
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style=" border: 2px solid white; border-radius: 25%;  display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/newmenumakangratis.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+        <div style="display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/css/fe_css/images/icons/menuandroid/applaporan.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style=" border: 2px solid white; border-radius: 25%;  display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/newmenuplus.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+        <div style="  display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/css/fe_css/images/icons/menuandroid/appplus.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 
     <a href="/comingsoon" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style=" border: 2px solid white; border-radius: 25%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/newmenusettings.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+        <div style="display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/css/fe_css/images/icons/menuandroid/appsettings.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
   
     <a href="javascript:history.back()" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="border: 2px solid white; border-radius: 25%; display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+        <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="/assets/css/fe_css/images/icons/menuandroid/menuback.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
