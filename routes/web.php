8ca0140f86e8414b16167051f148d3ec2b9beac4
Sigimatika <sigitechindo@gmail.com>
2024-05-29 23:13:25 +0700

    f

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/comingsoon', function () {
 // ----------------------------------------------------
 // ROUTE UNTUK PANGAMBILAN DATA PENANGGUNGJAWAB
 Route::resource('/', ProgrammakangratisController::class);
-Route::resource('/dashboard', ProgrammakangratisController::class);
+// Route::resource('/dashboard', ProgrammakangratisController::class);
 // ----------------------------------------------------
 
 // ----------------------------------------------------
