d389d7cf42af5d81c05e0eb7773efb89fc09aee6
Sigimatika <sigitechindo@gmail.com>
2024-05-30 13:42:53 +0700

    dgdg

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -50,4 +50,15 @@ class LoginController extends Controller
         // dd('Berhasil Masuk Iqlima');
     }
 
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
 }
--- resources/views/fe_dashboard/menu/android.blade.php
+++ resources/views/fe_dashboard/menu/android.blade.php
@@ -24,11 +24,25 @@
         </div>
     </a>
   
+    
+    @if(auth()->check()) 
+    
+    <form action="/logout" method="post" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
+        @csrf
+        <button type="submit" class="btn-2" style="background: none; border: none; padding: 0; margin: 0;"><img src="/assets/css/fe_css/images/icons/menuandroid/menulogout.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;"></button>
+    </form>
+    
+    
+    @else
+    <!-- Jika pengguna belum login -->
     <a href="/logins" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
         <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
             <img src="/assets/css/fe_css/images/icons/menuandroid/menulogin.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
+    
+    @endif
+
     
   
    
