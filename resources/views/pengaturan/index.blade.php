97f8d5bb182be977f96e88c62a21ecd8574fb8df
Sigimatika <sigitechindo@gmail.com>
2024-06-05 01:04:34 +0700

    egr

--- resources/views/pengaturan/index.blade.php
+++ resources/views/pengaturan/index.blade.php
@@ -122,9 +122,9 @@
                         <a href="/profile/{{$data->name}}">
                             <button type="submit" style="margin-left:75px; margin-bottom:10px; display: inline-block; padding: 10px 22px; font-weight: bold; font-size: 14px; color: #0a0a0a; border-radius: 9999px; width:250px; background-color: #efeff3; line-height: 18px; transition: background-color 0.3s, color 0.3s;" onmouseover="this.style.backgroundColor='#000'; this.style.color='#fff';" onmouseout="this.style.backgroundColor='#000000'; this.style.color='#fff'; text-align:center;"><i class="fas fa-user" style="margin-right: 5px;"></i> Profile</button>
                         </a>
-                        
+
                         @endforeach
-                        <a href="">
+                        <a href="/commingsoon">
                             <button type="submit" style="margin-left:75px; margin-bottom:10px; display: inline-block; padding: 10px 22px; font-weight: bold; font-size: 14px; color: #0a0a0a; border-radius: 9999px; width:250px; background-color: #efeff3; line-height: 18px; transition: background-color 0.3s, color 0.3s;" onmouseover="this.style.backgroundColor='#000'; this.style.color='#fff';" onmouseout="this.style.backgroundColor='#000000'; this.style.color='#fff'; text-align:center;"><i class="fas fa-utensil-spoon" style="margin-right: 5px;"></i> Daftar Menu</button>
                         </a>
                     </div>               
