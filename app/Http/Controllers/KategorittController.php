e96d4f95bd9b54e6654d71e04ecf4de9a9cc97da
Sigimatika <sigitechindo@gmail.com>
2024-06-06 15:20:00 +0700

    penambahan baru

--- app/Http/Controllers/KategorittController.php
+++ app/Http/Controllers/KategorittController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FundraisingController extends Controller
+class KategorittController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         //
--- config/app.php
+++ config/app.php
@@ -185,4 +185,8 @@ return [
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
+
+    
+    
+
 ];
