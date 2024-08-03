1b9a781bb6b099fb07b2e23685b1f299f2cfd898
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-08-03 17:33:10 +0700

    dfg

--- resources/views/fe_dashboard/menu/android.blade.php
+++ resources/views/fe_dashboard/menu/android.blade.php
@@ -15,7 +15,7 @@
         @if(auth()->check() && auth()->user()->is_admin === 'super_admin') 
     <a href="/admindashboard" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
         <div style="display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/tentangkami/androiddashboardadmin.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+            <img src="/assets/android/dashboard.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 @else
@@ -26,34 +26,35 @@
   
     <a href="/" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
         <div style=" solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/androidhome.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+            <img src="/assets/android/home.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
   
-    <a href="/404" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/androiddonasi.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+    <a href="/" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
+        <div style=" solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/android/pesanan.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+        </div>
+    </a>
+    
+    <a href="/" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
+        <div style=" solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/android/book.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
 
-    <a href="/404" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="  display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/icondonasi.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+    <a href="/" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
+        <div style=" solf display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
+            <img src="/assets/android/profil.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
-
-    <a href="/404" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
-        <div style="display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/androidsettings.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
-        </div>
-    </a>
+    
   
     
     @if(auth()->check()) 
     
     <form action="/logout" method="post" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
         @csrf
-        <button type="submit" class="btn-2" style="background: none; border: none; padding: 0; margin: 0;"><img src="/assets/css/fe_css/images/icons/menuandroid/menulogout.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;"></button>
+        <button type="submit" class="btn-2" style="background: none; border: none; padding: 0; margin: 0;"><img src="/assets/android/logout.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;"></button>
     </form>
     
     
@@ -61,7 +62,7 @@
     <!-- Jika pengguna belum login -->
     <a href="/404" class="flex items-center justify-center w-[50px] h-[50px] p-[13px_15px]">
         <div style=" display: flex; align-items: center; justify-content: center; overflow: hidden; font-size:100px">
-            <img src="/assets/css/fe_css/images/icons/menuandroid/menulogin.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
+            <img src="/assets/android/login.png" alt="icon" style="width: 100%; height: 100%; object-fit: cover;">
         </div>
     </a>
     
