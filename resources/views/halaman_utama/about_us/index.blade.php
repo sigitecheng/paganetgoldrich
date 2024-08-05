e367aabc59dc29b977ce037b07b7906054c96356
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-08-05 19:40:05 +0700

    dfg

--- resources/views/halaman_utama/about_us/index.blade.php
+++ resources/views/halaman_utama/about_us/index.blade.php
@@ -42,7 +42,7 @@
                         <img src="/assets/menuicon/LOGOGR.png" class="w-full h-full object-cover" alt="thumbnail" style="margin-left:10px; width:80px; height:80px; margin-top:20px;">
                     </div>
                     <div class="flex flex-col gap-1" style="margin-left: 15px; font-size:14px;">
-                        <p class="font-semibold text-white border-spacing-5" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Goldrich Renergybar </span> adalah protein bar superfood yang dirancang untuk memberikan energi maksimal dengan manfaat kesehatan yang luar biasa. Menggabungkan bahan-bahan bergizi dari biji-bijian, kacang, dan buah kering, produk ini menawarkan campuran ideal dari protein, serat, dan antioksidan. Dengan kandungan nutrisi yang padat dan kemasan yang praktis, Goldrich Renergybar adalah solusi sempurna untuk mendukung aktivitas fisik dan gaya hidup sehat Anda. </p>
+                        <p class="font-semibold text-white border-spacing-5 text-xl leading-[12px]" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Goldrich Renergybar </span> adalah protein bar superfood yang dirancang untuk memberikan energi maksimal dengan manfaat kesehatan yang luar biasa. Menggabungkan bahan-bahan bergizi dari biji-bijian, kacang, dan buah kering, produk ini menawarkan campuran ideal dari protein, serat, dan antioksidan. Dengan kandungan nutrisi yang padat dan kemasan yang praktis, Goldrich Renergybar adalah solusi sempurna untuk mendukung aktivitas fisik dan gaya hidup sehat Anda. </p>
                     </div>
                 </div>
             </a>
@@ -54,7 +54,7 @@
                         <img src="/assets/menuutama/4.png" class="w-full h-full object-cover" alt="thumbnail" style="border-radius:15px; margin-left:10px; width:80px; height:80px; margin-top:20px;">
                     </div>
                     <div class="flex flex-col gap-1" style="margin-left: 15px; font-size:14px;">
-                        <p class="font-semibold text-white border-spacing-5" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Granula Bar</span> adalah camilan praktis yang menggabungkan bahan-bahan bergizi dalam satu paket. Umumnya terbuat dari campuran biji-bijian, kacang-kacangan, dan buah kering yang dipadatkan dengan madu atau sirup sebagai pengikat, granula bar menjadi pilihan populer untuk mengisi waktu lapar dengan cara yang sehat. Camilan ini menawarkan berbagai manfaat kesehatan berkat kandungan serat, protein, dan vitamin yang tinggi, serta rendah kalori jika dibandingkan dengan snack olahan lainnya. Selain itu, granula bar juga mudah dibawa dan disimpan, menjadikannya solusi ideal untuk gaya hidup yang sibuk namun tetap ingin menjaga pola makan yang seimbang</p>
+                        <p class="font-semibold text-white border-spacing-5 text-xl leading-[12px]" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Granula Bar</span> adalah camilan praktis yang menggabungkan bahan-bahan bergizi dalam satu paket. Umumnya terbuat dari campuran biji-bijian, kacang-kacangan, dan buah kering yang dipadatkan dengan madu atau sirup sebagai pengikat, granula bar menjadi pilihan populer untuk mengisi waktu lapar dengan cara yang sehat. Camilan ini menawarkan berbagai manfaat kesehatan berkat kandungan serat, protein, dan vitamin yang tinggi, serta rendah kalori jika dibandingkan dengan snack olahan lainnya. Selain itu, granula bar juga mudah dibawa dan disimpan, menjadikannya solusi ideal untuk gaya hidup yang sibuk namun tetap ingin menjaga pola makan yang seimbang</p>
                     </div>
                 </div>
             </a>
@@ -66,7 +66,7 @@
                         <img src="/assets/menuutama/5.png" class="w-full h-full object-cover" alt="thumbnail" style="border-radius:15px; margin-left:10px; width:80px; height:80px; margin-top:20px;">
                     </div>
                     <div class="flex flex-col gap-1" style="margin-left: 15px; font-size:14px;">
-                        <p class="font-semibold text-white border-spacing-5" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Protein Bar</span> 
+                        <p class="font-semibold text-white border-spacing-5 text-xl leading-[12px]" style="margin-top:30px; text-align:justify; margin-bottom:10px;"><span style="color:brown;">Protein Bar</span> 
                             adalah camilan yang dirancang khusus untuk meningkatkan asupan protein dalam diet sehari-hari, ideal bagi mereka yang aktif atau yang membutuhkan tambahan protein untuk mendukung pemulihan otot dan kesehatan umum. Biasanya terbuat dari campuran protein whey, kasein, atau sumber protein nabati seperti kedelai dan pea, protein bar juga sering diperkaya dengan vitamin, mineral, dan serat. Meskipun bervariasi dalam rasa dan kandungan nutrisi, protein bar umumnya dirancang untuk memberikan energi yang tahan lama serta membantu menstabilkan kadar gula darah. Dengan kemasan yang praktis dan portabel, protein bar adalah pilihan populer sebagai camilan pasca-latihan atau sebagai alternatif makan siang cepat bagi mereka yang memiliki jadwal padat.
                         </div>
                 </div>
